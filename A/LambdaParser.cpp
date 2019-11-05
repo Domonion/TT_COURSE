6ccb91d1c84b6c1c90c9f0845f8bef6c27143b5a
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2019-11-05 13:06:19 +0300

    reorder for simplicity

--- A/LambdaParser.cpp
+++ A/LambdaParser.cpp
@@ -6,25 +6,21 @@
 #include "Util.h"
 #include "main.h"
 
-LambdaParser::TreeNode::~TreeNode() {}
-
-void LambdaParser::whitespaceToSpace(std::string &out) {
-    foraa(i, out) {
-        if (isspace(i)) {
-            i = ' ';
+int findClosed(string const &input, int startInd = 0) {
+    if (input[startInd] == Util::OPEN) {
+        int balance = 1;
+        forn(i, startInd + 1, size(input)) {
+            if (input[i] == Util::CLOSE && --balance == 0) {
+                return i;
+            } else if (input[i] == Util::OPEN) {
+                ++balance;
+            }
         }
     }
+    return -1;
 }
 
-void LambdaParser::trimSpaces(std::string &out) {
-    out.resize(unique(all(out)) - out.begin());
-}
-
-LambdaParser::Expression *LambdaParser::Parse(std::string input) {
-    whitespaceToSpace(input);
-    trimSpaces(input);
-    return Expression::Create(input);
-}
+LambdaParser::TreeNode::~TreeNode() {}
 
 LambdaParser::Variable::Variable(std::string const &_name) : name(_name) {}
 
@@ -60,38 +56,12 @@ LambdaParser::Variable::~Variable() {}
 
 LambdaParser::Atom::Atom(LambdaParser::TreeNode *const _value) : value(_value) {}
 
-std::string const &LambdaParser::Atom::ToString() const {
-    if (is(value, Expression *const, expr)) {
-        return Util::GetInstance().OPEN + expr->ToString() + Util::GetInstance().CLOSE;
-    } else if (is(value, Variable *const, var)) {
-        return var->ToString();
-    } else {
-        throw new ParserException("Atom::ToString - Impossible cast", this);
+LambdaParser::Atom *LambdaParser::Atom::Create(std::string const &input) {
+    LambdaParser::TreeNode *var = nullptr;
+    if (!TryCreate(input, var)) {
+        throw ParserException("didn't create Atom from input: " + input, nullptr);
     }
-}
-
-LambdaParser::Atom::~Atom() {}
-
-LambdaParser::TreeNode *LambdaParser::Atom::GetValue() const {
-    return value;
-}
-
-bool LambdaParser::Atom::IsVariable() const {
-    return dynamic_cast<Variable *>(value);
-}
-
-int findClosed(string const &input, int startInd = 0) {
-    if (input[startInd] == Util::OPEN) {
-        int balance = 1;
-        forn(i, startInd + 1, size(input)) {
-            if (input[i] == Util::CLOSE && --balance == 0) {
-                return i;
-            } else if (input[i] == Util::OPEN) {
-                ++balance;
-            }
-        }
-    }
-    return -1;
+    return dynamic_cast<LambdaParser::Atom *>(var);
 }
 
 bool LambdaParser::Atom::TryCreate(std::string const &input, LambdaParser::TreeNode *&var) {
@@ -111,43 +81,72 @@ bool LambdaParser::Atom::TryCreate(std::string const &input, LambdaParser::TreeN
     return done;
 }
 
-LambdaParser::Atom *LambdaParser::Atom::Create(std::string const &input) {
+bool LambdaParser::Atom::IsVariable() const {
+    return dynamic_cast<Variable *>(value);
+}
+
+std::string const &LambdaParser::Atom::ToString() const {
+    if (is(value, Expression *const, expr)) {
+        return Util::GetInstance().OPEN + expr->ToString() + Util::GetInstance().CLOSE;
+    } else if (is(value, Variable *const, var)) {
+        return var->ToString();
+    } else {
+        throw new ParserException("Atom::ToString - Impossible cast", this);
+    }
+}
+
+LambdaParser::TreeNode *LambdaParser::Atom::GetValue() const {
+    return value;
+}
+
+LambdaParser::Atom::~Atom() {}
+
+LambdaParser::Use::Use(LambdaParser::Atom *const _value, LambdaParser::Use *const _next) : value(_value), next(_next) {
+
+}
+
+LambdaParser::Use *LambdaParser::Use::Create(std::string const &input) {
     LambdaParser::TreeNode *var = nullptr;
     if (!TryCreate(input, var)) {
         throw ParserException("didn't create Atom from input: " + input, nullptr);
     }
-    return dynamic_cast<LambdaParser::Atom *>(var);
+    return dynamic_cast<LambdaParser::Use *>(var);
+}
+
+std::string const &LambdaParser::Use::ToString() const {
+    if (next == nullptr)
+        return value->ToString();
+    else {
+        return Util::GetInstance().OPEN + next->ToString() + " " + value->ToString() + Util::GetInstance().CLOSE;
+    }
+}
+
+LambdaParser::Atom *LambdaParser::Use::GetValue() const {
+    return value;
+}
+
+bool LambdaParser::Use::IsLast() const {
+    return next == nullptr;
+}
+
+LambdaParser::Use *LambdaParser::Use::GetNext() const {
+    return next;
+}
+
+LambdaParser::Use::~Use() {
+
 }
 
 ////TODO is it true that plain Variable * will pass?
 //todo static threadlocal random in util
 ////todo 2. for use stop sumbols are: \ and eof
 
-LambdaParser::ParserException::ParserException(std::string const &_info, const LambdaParser::TreeNode *_node)
-        : exception(), info(_info), node(_node) {}
-
-LambdaParser::Expression::~Expression() {
-
-}
-
-LambdaParser::Expression *LambdaParser::Expression::GetExpression() const {
-    return expr;
-}
-
-LambdaParser::Variable *LambdaParser::Expression::GetVariable() const {
-    return variable;
-}
-
-LambdaParser::Use *LambdaParser::Expression::GetUsage() const {
-    return usage;
-}
-
-bool LambdaParser::Expression::IsUsage() const {
-    return expr == nullptr;
-}
-
-bool LambdaParser::Expression::IsClosed() const {
-    return !IsUsage() && usage == nullptr;
+LambdaParser::Expression *LambdaParser::Expression::Create(std::string const &input) {
+    LambdaParser::TreeNode *var = nullptr;
+    if (!TryCreate(input, var)) {
+        throw ParserException("didn't create Atom from input: " + input, nullptr);
+    }
+    return dynamic_cast<LambdaParser::Expression *>(var);
 }
 
 std::string const &LambdaParser::Expression::ToString() const {
@@ -159,52 +158,47 @@ std::string const &LambdaParser::Expression::ToString() const {
     }
 }
 
-LambdaParser::Expression *LambdaParser::Expression::Create(std::string const &input) {
-    LambdaParser::TreeNode *var = nullptr;
-    if (!TryCreate(input, var)) {
-        throw ParserException("didn't create Atom from input: " + input, nullptr);
+LambdaParser::Use *LambdaParser::Expression::GetUsage() const {
+    return usage;
+}
+
+LambdaParser::Variable *LambdaParser::Expression::GetVariable() const {
+    return variable;
+}
+
+LambdaParser::Expression *LambdaParser::Expression::GetExpression() const {
+    return expr;
+}
+
+bool LambdaParser::Expression::IsUsage() const {
+    return expr == nullptr;
+}
+
+bool LambdaParser::Expression::IsClosed() const {
+    return !IsUsage() && usage == nullptr;
+}
+
+LambdaParser::Expression::~Expression() {
+
+}
+
+LambdaParser::Expression *LambdaParser::Parse(std::string input) {
+    whitespaceToSpace(input);
+    trimSpaces(input);
+    return Expression::Create(input);
+}
+
+void LambdaParser::whitespaceToSpace(std::string &out) {
+    foraa(i, out) {
+        if (isspace(i)) {
+            i = ' ';
+        }
     }
-    return dynamic_cast<LambdaParser::Expression *>(var);
 }
 
-LambdaParser::Use::~Use() {
-
+void LambdaParser::trimSpaces(std::string &out) {
+    out.resize(unique(all(out)) - out.begin());
 }
 
-LambdaParser::Use *LambdaParser::Use::GetNext() const {
-    return next;
-}
-
-bool LambdaParser::Use::IsLast() const {
-    return next == nullptr;
-}
-
-LambdaParser::Atom *LambdaParser::Use::GetValue() const {
-    return value;
-}
-
-LambdaParser::Use::Use(LambdaParser::Atom *const _value, LambdaParser::Use *const _next) : value(_value), next(_next) {
-
-}
-
-std::string const &LambdaParser::Use::ToString() const {
-    if (next == nullptr)
-        return value->ToString();
-    else {
-        return Util::GetInstance().OPEN + next->ToString() + " " + value->ToString() + Util::GetInstance().CLOSE;
-    }
-}
-
-LambdaParser::Use *LambdaParser::Use::Create(std::string const &input) {
-    LambdaParser::TreeNode *var = nullptr;
-    if (!TryCreate(input, var)) {
-        throw ParserException("didn't create Atom from input: " + input, nullptr);
-    }
-    return dynamic_cast<LambdaParser::Use *>(var);
-}
-
-bool LambdaParser::Use::TryCreate(std::string const &input, LambdaParser::TreeNode *&var) {
-    return false;
-}
-
-
+LambdaParser::ParserException::ParserException(std::string const &_info, const LambdaParser::TreeNode *_node)
+        : exception(), info(_info), node(_node) {}
