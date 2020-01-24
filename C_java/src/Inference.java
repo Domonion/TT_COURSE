502201546f8f3d8060fbc06997751202d1246a79
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2020-01-24 12:53:09 +0300

    continue refactoring lol

--- C_java/src/Inference.java
+++ C_java/src/Inference.java
@@ -72,22 +72,18 @@ public class Inference {
 
     private boolean Replace(TypeNode typeNode, IType toFind, IType toReplace) {
         boolean res;
-        //todo invert if
-        if (typeNode.left instanceof Type) {
-            //todo invert if
-            if (typeNode.left == toFind) {
+        if (!(typeNode.left instanceof Type)) {
+            res = Replace((TypeNode) typeNode.left, toFind, toReplace);
+        } else {
+            if (typeNode.left != toFind) {
+                res = false;
+            } else {
                 typeNode.left = toReplace;
                 res = true;
-            } else {
-                res = false;
             }
-        } else {
-            res = Replace((TypeNode) typeNode.left, toFind, toReplace);
         }
         if (!res) {
-            //todo invert if
             if (typeNode.right instanceof Type) {
-                //todo invert if
                 if (typeNode.right == toFind) {
                     typeNode.right = toReplace;
                     res = true;
@@ -102,92 +98,75 @@ public class Inference {
     private boolean Replace(Type toFind, IType toReplace) {
         AtomicBoolean result = new AtomicBoolean(false);
         List<Pair<IType, IType>> addition = new LinkedList<>();
-        List<Pair<IType, IType>> removement = new LinkedList<>();
+        List<Pair<IType, IType>> removals = new LinkedList<>();
         typesList.forEach(current -> {
-            //todo invert if
-            if (!(current.getKey() == toFind && current.getValue() == toReplace)) {
-                //todo invert if
-                if (current.getKey() instanceof Type) {
-                    //todo invert if
-                    if (current.getKey() == toFind) {
-                        removement.add(current);
-                        addition.add(new Pair<>(toReplace, current.getValue()));
-                        result.set(true);
-                    }
-                } else if (current.getKey() instanceof TypeNode) {
-                    //todo invert if
-                    if (Replace((TypeNode) current.getKey(), toFind, toReplace)) {
-                        result.set(true);
-                    }
+            if (current.getKey() == toFind && current.getValue() == toReplace) {
+                return;
+            }
+            if (current.getKey() instanceof TypeNode) {
+                boolean bool = Replace((TypeNode) current.getKey(), toFind, toReplace);
+                if (bool) {
+                    result.set(true);
                 }
-                //todo invert if
-                if (current.getValue() instanceof Type) {
-                    //todo invert if
-                    if (current.getValue() == toFind) {
-                        removement.add(current);
-                        addition.add(new Pair<>(current.getKey(), toReplace));
-                        result.set(true);
-                    }
-                } else if (current.getValue() instanceof TypeNode) {
-                    //todo invert if
-                    if (Replace((TypeNode) current.getValue(), toFind, toReplace)) {
-                        result.set(true);
-                    }
+            } else if (current.getKey() instanceof Type) {
+                if (current.getKey() == toFind) {
+                    removals.add(current);
+                    addition.add(new Pair<>(toReplace, current.getValue()));
+                    result.set(true);
+                }
+            }
+            if (current.getValue() instanceof TypeNode) {
+                boolean bool = Replace((TypeNode) current.getValue(), toFind, toReplace);
+                if (bool) {
+                    result.set(true);
+                }
+            } else if (current.getValue() instanceof Type) {
+                if (current.getValue() == toFind) {
+                    removals.add(current);
+                    addition.add(new Pair<>(current.getKey(), toReplace));
+                    result.set(true);
                 }
             }
         });
         typesList.addAll(addition);
-        typesList.removeAll(removement);
+        typesList.removeAll(removals);
         return result.get();
     }
 
     private void Start(TypeNode typeNode, IType toFind, IType toReplace) {
-        //todo invert if
         if (typeNode.left instanceof Type) {
-            //todo invert if
             if (typeNode.left == toFind) {
                 typeNode.left = toReplace;
             }
         } else if (typeNode.left instanceof TypeNode) {
             Replace((TypeNode) typeNode.left, toFind, toReplace);
         }
-        //todo invert if
-        if (typeNode.right instanceof Type) {
-            //todo invert if
+        if (typeNode.right instanceof TypeNode) {
+            Replace((TypeNode) typeNode.right, toFind, toReplace);
+        } else if (typeNode.right instanceof Type) {
             if (typeNode.right == toFind) {
                 typeNode.right = toReplace;
             }
-        } else if (typeNode.right instanceof TypeNode) {
-            Replace((TypeNode) typeNode.right, toFind, toReplace);
         }
     }
 
     private boolean Unificate() {
         List<Pair<IType, IType>> addition = new LinkedList<>();
+        int counter;
+        AtomicInteger typeCount = new AtomicInteger();
         while (true) {
             if (Check()) return false;
-            //todo declaration
             typesList.addAll(addition);
             addition.clear();
-            int counter = 0;
-            AtomicInteger typeCount = new AtomicInteger();
+            counter = 0;
+            typeCount.set(0);
             List<Pair<IType, IType>> entries = new ArrayList<>(typesList);
             for (Pair<IType, IType> entry : entries) {
-                //todo invert if
-                if (entry.getKey() != entry.getValue()) {
-                    //todo invert if
-                    if (entry.getKey() instanceof Type) {
-                        typeCount.getAndIncrement();
-                        if (Replace((Type) entry.getKey(), entry.getValue())) {
-                            counter++;
-                            break;
-                        }
-                    } else if (entry.getKey() instanceof TypeNode) {
-                        //todo invert if
-                        if (entry.getValue() instanceof Type) {
-                            typesList.remove(entry);
-                            addition.add(new Pair<>(entry.getValue(), entry.getKey()));
-                        } else if (entry.getValue() instanceof TypeNode) {
+                if (entry.getKey() == entry.getValue()) {
+                    typesList.remove(entry);
+                } else {
+                    if (entry.getKey() instanceof TypeNode) {
+                        if (entry.getValue() instanceof TypeNode) {
                             IType l = ((TypeNode) entry.getKey()).left;
                             IType r = ((TypeNode) entry.getKey()).right;
                             IType a = ((TypeNode) entry.getValue()).left;
@@ -195,10 +174,17 @@ public class Inference {
                             addition.add(new Pair<>(l, a));
                             addition.add(new Pair<>(r, b));
                             typesList.remove(entry);
+                        } else if (entry.getValue() instanceof Type) {
+                            typesList.remove(entry);
+                            addition.add(new Pair<>(entry.getValue(), entry.getKey()));
+                        }
+                    } else if (entry.getKey() instanceof Type) {
+                        typeCount.getAndIncrement();
+                        if (Replace((Type) entry.getKey(), entry.getValue())) {
+                            counter++;
+                            break;
                         }
                     }
-                } else {
-                    typesList.remove(entry);
                 }
             }
             if (counter == 0 && typeCount.get() == typesList.size() + addition.size()) {
@@ -232,16 +218,15 @@ public class Inference {
             }
             index++;
         }
-        //todo change if
         typesStr.append(context.isEmpty() ? "" : " ");
         System.out.println(typesStr + "|- " + e + " " + t + " [rule #" + ruleNumber + "]");
-        if (expression instanceof Expression) {
+        if (expression instanceof Use) {
+            PrintAnswer(((Use) expression).left, context, depth + 1);
+            PrintAnswer(((Use) expression).right, context, depth + 1);
+        } else if (expression instanceof Expression) {
             HashSet<Variable> kek = new HashSet<>(context);
             kek.add(((Expression) expression).x);
             PrintAnswer(((Expression) expression).e, kek, depth + 1);
-        } else if (expression instanceof Use) {
-            PrintAnswer(((Use) expression).left, context, depth + 1);
-            PrintAnswer(((Use) expression).right, context, depth + 1);
         }
     }
 
@@ -253,20 +238,18 @@ public class Inference {
         }
         typesList.forEach(pair -> newTypes.put(pair.getKey(), pair.getValue()));
         types.keySet().forEach(current -> {
-            //todo type declaration
             IType type = types.get(current);
             if (newTypes.containsKey(type)) {
                 types.put(current, newTypes.get(type));
             }
         });
         typesList.forEach(pair -> types.forEach((key, value) -> {
-            //todo change if
-            if (value instanceof Type) {
+            if (value instanceof TypeNode) {
+                Start((TypeNode) value, pair.getKey(), pair.getValue());
+            } else if (value instanceof Type) {
                 if (value == pair.getKey()) {
                     types.put(key, pair.getValue());
                 }
-            } else if (value instanceof TypeNode) {
-                Start((TypeNode) value, pair.getKey(), pair.getValue());
             }
         }));
         PrintAnswer(expression, vars.stream().filter(cur -> cur.can).collect(Collectors.toSet()), 0);
